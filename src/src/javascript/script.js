2a66f2aa2d9adb403becdf3b8091174f4ebc0528
Ivoneideduarte <67153931+Ivoneideduarte@users.noreply.github.com>
2026-04-03 12:39:18 -0300

    Validações

--- src/src/javascript/script.js
+++ src/src/javascript/script.js
@@ -1,3 +1,236 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    /*
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const confirmPassword = document.querySelector('#confirm_password').value;
+    */
+
+    const fields = [
+        {
+            id: 'name',
+            label: 'Nome',
+            validator: nameIsValid
+        },
+        {
+            id: 'last_name',
+            label: 'Sobrenome',
+            validator: nameIsValid
+        },
+        {
+            id: 'birthdate',
+            label: 'Nascimento',
+            validator: dateIsValid
+        },
+        {
+            id: 'email',
+            label: 'Email',
+            validator: emailIsValid
+        },
+        {
+            id: 'password',
+            label: 'Senha',
+            validator: passwordIsSecure
+        },
+        {
+            id: 'confirm_password',
+            label: 'Confirmar Senha',
+            validator: passwordMatch
+        }
+    ]
+
+    const errorIcon = '<i class="fa-solid fa-triangle-exclamation"></i>';
+
+    fields.forEach(function (field) {
+        const input = document.getElementById(field.id);
+        const inputBox = input.closest('.input-box');
+        const inputValue = input.value;
+        const errorSpan = inputBox.querySelector('.error');
+        errorSpan.innerHTML = '';
+        inputBox.classList.remove('invalid');
+        inputBox.classList.add('valid');
+
+        const fieldValidation = field.validator(inputValue);
+        if (!fieldValidation.isValid) {
+            errorSpan.innerHTML = `${errorIcon} ${fieldValidation.errorMessage}`;
+            inputBox.classList.add('invalid');
+            inputBox.classList.remove('valid');
+            return;
+        }
+
+    })
+
+    const genders = document.getElementsByName('gender');
+    const radioContainer = document.querySelector('.radio-container');
+    const genderErrorSpan = radioContainer.querySelector('.error');
+
+    const selectedGender = [...genders].find(input => input.checked);
+
+    radioContainer.classList.add('invalid');
+    radioContainer.classList.remove('valid');
+    genderErrorSpan.innerHTML = `${errorIcon} Selecione um gênero!`;
+
+    if (selectedGender) {
+        radioContainer.classList.add('valid');
+        radioContainer.classList.remove('invalid');
+        genderErrorSpan.innerHTML = '';
+        return;
+    }
+
+    function isEmpty(value) {
+        return value === '';
+    }
+
+    function nameIsValid(value) {
+
+        const validator = {
+            isValid: true,
+            errorMessage: null
+        }
+
+
+        if (isEmpty(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'O campo é obrigatório.';
+            return validator;
+        }
+
+        const min = 3;
+        if (value.length < min) {
+            validator.isValid = false;
+            validator.errorMessage = `O campo deve conter pelo menos ${min} caracteres.`;
+            return validator;
+        }
+
+        const regex = /^[a-zA-Z\s]+$/;
+        if (!regex.test(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'O campo deve conter apenas letras e espaços.';
+        }
+        return validator;
+    }
+
+    function dateIsValid(value) {
+
+        const validator = {
+            isValid: true,
+            errorMessage: null
+        }
+        if (isEmpty(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'O nascimento é obrigatório.';
+            return validator;
+        }
+
+        const year = new Date(value).getFullYear();
+
+        if (year < 1920 || year > new Date().getFullYear()) {
+            validator.isValid = false;
+            validator.errorMessage = 'Data de nascimento inválida.';
+            return validator;
+        }
+        return validator;
+    }
+
+    function emailIsValid(value) {
+
+        const validator = {
+            isValid: true,
+            errorMessage: null
+        }
+
+        if (isEmpty(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'O email é obrigatório.';
+            return validator;
+        }
+
+        const regex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
+        if (!regex.test(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'O email precisa ser válido.';
+        }
+        return validator;
+    }
+
+    function passwordIsSecure(value) {
+
+        const validator = {
+            isValid: true,
+            errorMessage: null
+        }
+        if (isEmpty(value)) {
+            validator.isValid = false;
+            validator.errorMessage = 'A senha é obrigatória.';
+            return validator;
+        }
+
+        const regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$');
+        if (!regex.test(value)) {
+            validator.isValid = false;
+            validator.errorMessage = `
+                Sua senha deve conter ao menos: <br/>
+                8 dígitos <br/>
+                1 letra maiúscula <br/>
+                1 letra minúscula <br/>
+                1 número <br/>
+                1 caractere especial! <br/>
+            `;
+            return validator;
+        }
+
+        return validator;
+    }
+
+    function passwordMatch(value) {
+        const validator = {
+            isValid: true,
+            errorMessage: null
+        }
+
+        const passwordValue = document.getElementById('password').value;
+
+        if (value === '' || passwordValue !== value) {
+            validator.isValid = false;
+            validator.errorMessage = 'As senhas não coincidem.';
+            return validator;
+        }
+
+        return validator;
+    };
+});
+
+
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirm_password');
+
+function validateField(input, validator) {
+    const inputBox = input.closest('.input-box');
+    const errorSpan = inputBox.querySelector('.error');
+    const validation = validator(input.value);
+    errorSpan.innerHTML = '';
+    inputBox.classList.remove('invalid', 'valid');
+    if (!validation.isValid) {
+        errorSpan.innerHTML = `${errorIcon} ${validation.errorMessage}`;
+        inputBox.classList.add('invalid');
+    } else {
+        inputBox.classList.add('valid');
+    }
+}
+
+passwordInput.addEventListener('input', function () {
+    validateField(passwordInput, fieldIsRequired);
+    validateField(confirmPasswordInput, passwordMatch);
+});
+
+confirmPasswordInput.addEventListener('input', function () {
+    validateField(confirmPasswordInput, passwordMatch);
+});
+
+
 const passwordIcons = document.querySelectorAll('.password-icon');
 
 passwordIcons.forEach(icon => {
@@ -8,3 +241,5 @@ passwordIcons.forEach(icon => {
         this.classList.toggle('fa-eye');
     });
 });
+
+
